f0cacc29b9b76e00946c07ece7314a5040983820
Divyoj Singh <divyojs@gmail.com>
2022-04-13 14:43:05 +0530

    Toggle Switch plus self activations

---
+++ Toggle Switch plus Self Activation/Figure 3/H/Vectorised 2D Self Activation.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 20 21:08:56 2021
+
+@author: N.V
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 20 18:47:06 2021
+
+@author: N.V
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.animation import FuncAnimation
+import os
+# note that this must be executed before 'import numba'
+os.environ['NUMBA_DISABLE_INTEL_SVML'] = '1'
+from numba import njit
+import time as process_time
+
+ #Degradtion rate
+gamma_A=0.1; gamma_B=0.1;
+#Transcription rate
+g_A=5 ; g_B=5;
+
+
+# @njit
+def hill(X,X0,l,n):
+    #H+
+    H1=(X**n)/(X0**n+X**n)
+    #H-
+    H2=(X0**n)/(X**n+X0**n)
+    #Hill function =H-(+) lambda*H+
+    H=H2+l*H1
+    return H
+
+def interactions_sa(X):
+    dXdt=np.zeros(np.shape(X))
+    X=np.asarray(X)
+   
+    #Degradtion rate
+    gamma_A=0.1; gamma_B=0.1;
+    #Transcription rate
+    g_A=5 ; g_B=5;
+    #Hills function threshold 
+    A0B=120 ; B0A=120;
+    A0A=80;B0B=80;
+    
+    #Co-operativity 
+    nAtoB=1;
+    nBtoA=1;
+    nAtoA=4;
+    nBtoB=4;
+    
+    
+    #fold change
+    lambda_AtoB =0.1
+    lambda_BtoA=0.1
+    lambda_AtoA=5
+    lambda_BtoB=5
+    
+    #Equation=
+    dXdt[0,:,:]=g_A*hill(X[0,:,:],A0A,lambda_AtoA,nAtoA)*hill(X[1,:,:],B0A,lambda_BtoA,nBtoA) -gamma_A*X[0,:,:]
+    dXdt[1,:,:]=g_B*hill(X[1,:,:],B0B,lambda_BtoB,nBtoB)*hill(X[0,:,:],A0B,lambda_AtoB,nAtoB) -gamma_B*X[1,:,:]
+    
+    return dXdt
+
+
+# dimsension size, mm
+w = h = 10.
+# intervals in x-, y- directions, mm
+dx = dy = 0.1
+#Diffusivity
+D = 0.0001
+alpha=10; gamma=0.5
+
+u_initial, v_initial = 100, 100
+T_max=1000;
+nx, ny = int(w/dx), int(h/dy)
+
+dx2, dy2 = dx*dx, dy*dy
+dt = 0.01; #dx2 * dy2 / (2 * D * (dx2 + dy2))
+resolution=10/dt;
+#Setting the initial conditions
+
+u0 = u_initial * np.ones((nx, ny))+ 10*np.random.random((nx, ny))
+v0 = v_initial * np.ones((nx, ny))+ 10*np.random.random((nx, ny))
+
+
+u = u0.copy();v = v0.copy()
+
+
+
+
+
+
+# @njit
+def do_timestep(u0, u, v0, v):
+    # Propagate with forward-difference in time, central-difference in space
+     X0= [ u0[1:-1,1:-1] , v0[1:-1,1:-1] ]
+    
+     F=interactions_sa(X0)
+    
+     u[1:-1, 1:-1] = u0[1:-1, 1:-1] + D * dt * ((u0[2:, 1:-1] - 2*u0[1:-1, 1:-1] + u0[:-2, 1:-1])/dx2+ (u0[1:-1, 2:] - 2*u0[1:-1, 1:-1] + u0[1:-1, :-2])/dy2 )+ dt*F[0,:,:] 
+    
+     v[1:-1, 1:-1] = v0[1:-1, 1:-1] + D * dt * ((v0[2:, 1:-1] - 2*v0[1:-1, 1:-1] + v0[:-2, 1:-1])/dx2+ (v0[1:-1, 2:] - 2*v0[1:-1, 1:-1] + v0[1:-1, :-2])/dy2 ) +dt*F[1,:,:]
+    
+    ## Boundary Condition along x axis:
+     u[0,:],u[-1,:]=u[1,:],u[-2,:]
+     v[0,:],v[-1,:]=v[1,:],v[-2,:]
+    
+    ## Boundary Condition along y axis:
+     u[:,0],u[:,-1]=u[:,1],u[:,-2]
+     v[:,0],v[:,-1]=v[:,1],v[:,-2]
+    
+    
+     u0 = u.copy();v0 = v.copy()
+     return u0, u, v0, v
+
+
+def plotconcmap(u_k, k):
+    # Clear the current plot figure
+    plt.clf()
+    plt.rcParams.update({'font.size':14})
+    # plt.title(f"A at t = {k:.3f} min",weight='bold')
+    plt.xlabel("x",weight='bold')
+    plt.ylabel("y",weight='bold')
+
+    # This is to plot u_k (u at time-step k)
+    plt.title("T = "+ str(int(k*resolution/10)))
+    plt.pcolormesh(u_k, cmap=plt.cm.jet, vmin=0, vmax=0.8 ,shading='gouraud')
+    plt.colorbar()
+    # plt.show()
+
+    return plt
+
+def animate(k):
+    plotconcmap(uk[k]/(g_A/gamma_A), k)
+
+
+# Number of timesteps
+nsteps = int(T_max/dt)
+
+t_start=process_time.time()
+uk = np.empty((nx, ny));vk=u.copy()
+for m in range(nsteps):
+    u0, u, v0, v = do_timestep(u0, u, v0, v)
+    
+    if (m%resolution)==0:
+        print("At time = ",m*dt)
+        vk=np.dstack((vk,v));
+        uk=np.dstack((uk,u));
+    
+uk=np.moveaxis(uk, -1, 0)
+vk=np.moveaxis(vk, -1, 0)
+print("Simulation time (in min)= ",(process_time.time()-t_start)/60)
+#%%
+
+anim = animation.FuncAnimation(plt.figure(), animate,interval=100,frames=np.size(uk,0),repeat=False)
+anim.save("Toggle_Switch_self_act_tri.mp4")
+
+print("Total time (in min)= ",(process_time.time()-t_start)/60)
---
+++ Toggle Switch plus Self Activation/Figure S5/B/interactions.m
@@ -0,0 +1,57 @@
+%% Code which contains the equations:
+function out = interactions
+out{1} = @init;
+out{2} = @fun_eval;
+out{3} = [];
+out{4} = [];
+out{5} = [];
+out{6} = [];
+out{7} = [];
+out{8} = [];
+out{9} = [];
+
+% --------------------------------------------------------------------------
+function dydt = fun_eval(t,kmrgd,gammaA,gammaB,gA,gB,A0B,A0A,lB0A,lA0A,nB0A,nA0A,B0A,B0B,lA0B,lB0B,nA0B,nB0B)
+%% The equations are entered here:
+% the order of parameters given to fun_eval should be conserved throughout
+% the codes. kmrgd contains the species, so kmrgd(1) = A. The equations are
+% seprated via ";" and there is no ; after the last equation. Instead you
+% should put it after closing the square bracket to supress diplaying the
+% values on screen. For example, for ORDINARY TOGGLE SWITCH:
+% dydt=[gA*hill(kmrgd(2),B0A,lB0A,nB0A)-gammaA*kmrgd(1);
+%        gB*hill(kmrgd(1),A0B,lA0B,nA0B)-gammaB*kmrgd(2)];
+%FOR SA_TOGGLE SWITCH :
+% dydt=[gA*hill(kmrgd(2),B0A,lB0A,nB0A)*hill(kmrgd(1),A0A,lA0A,nA0A)-gammaA*kmrgd(1);
+%        gB*hill(kmrgd(1),A0B,lA0B,nA0B)*hill(kmrgd(2),B0B,lB0B,nB0B)-gammaB*kmrgd(2)];
+%For TOGGLE TRIAD WITHOUT ACTIVATION :
+%   dydt=[ g_A*hill(kmrgd(2),B0A,lambda_BtoA,nBtoA)*hill(kmrgd(3),C0A,lambda_CtoA,nCtoA) - gamma_A*kmrgd(1); 
+%          g_B*hill(kmrgd(1),A0B,lambda_AtoB,nAtoB)*hill(kmrgd(3),C0B,lambda_CtoB,nCtoB) - gamma_B*kmrgd(2); 
+%          g_C*hill(kmrgd(1),A0C,lambda_AtoC,nAtoC)*hill(kmrgd(2),B0C,lambda_BtoC,nBtoC) - gamma_C*kmrgd(3)];
+
+dydt=[gA*hill(kmrgd(2),B0A,lB0A,nB0A)*hill(kmrgd(1),A0A,lA0A,nA0A)-gammaA*kmrgd(1);
+       gB*hill(kmrgd(1),A0B,lA0B,nA0B)*hill(kmrgd(2),B0B,lB0B,nB0B)-gammaB*kmrgd(2)];
+
+    
+% --------------------------------------------------------------------------
+function [tspan,y0,options] = init
+%% Let this be as it is:
+handles = feval(interactions);
+y0=[0,0,0,0];
+options = odeset('Jacobian',[],'JacobianP',[],'Hessians',[],'HessiansP',[]);
+tspan = [0 10];
+
+%% This part is also not touched (didn't change the results as of now)
+% --------------------------------------------------------------------------
+function jac = jacobian(t,kmrgd,s,gammaA,gammaB,gA,gB,B0,lB0A,nB0A,A0,lA0B,nA0B)
+% --------------------------------------------------------------------------
+function jacp = jacobianp(t,kmrgd,s,gammaA,gammaB,gA,gB,B0,lB0A,nB0A,A0,lA0B,nA0B)
+% --------------------------------------------------------------------------
+function hess = hessians(t,kmrgd,s,gammaA,gammaB,gA,gB,B0,lB0A,nB0A,A0,lA0B,nA0B)
+% --------------------------------------------------------------------------
+function hessp = hessiansp(t,kmrgd,s,gammaA,gammaB,gA,gB,B0,lB0A,nB0A,A0,lA0B,nA0B)
+%---------------------------------------------------------------------------
+function tens3  = der3(t,kmrgd,s,gammaA,gammaB,gA,gB,B0,lB0A,nB0A,A0,lA0B,nA0B)
+%---------------------------------------------------------------------------
+function tens4  = der4(t,kmrgd,s,gammaA,gammaB,gA,gB,B0,lB0A,nB0A,A0,lA0B,nA0B)
+%---------------------------------------------------------------------------
+function tens5  = der5(t,kmrgd,s,gammaA,gammaB,gA,gB,B0,lB0A,nB0A,A0,lA0B,nA0B)
