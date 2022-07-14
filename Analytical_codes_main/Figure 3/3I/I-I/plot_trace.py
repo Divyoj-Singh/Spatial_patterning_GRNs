0cfee12efb810dfad6a4bbff4b0f54baa65208cd
Divyoj Singh <divyojs@gmail.com>
2022-07-14 15:36:14 +0530

    Analytical codes updated

---
+++ Analytical_codes_main/Figure 3/3I/I-I/plot_trace.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x, y = np.loadtxt('TSSA_tri_trace_1D.dat', unpack=True, usecols=[0,1])
+
+plt.plot(x, y, label="1D diffusion", color='blue', linestyle='solid', linewidth = 4, marker='o', markerfacecolor='blue', markeredgecolor="navy", markersize=12)
+
+x, y = np.loadtxt('TSSA_tri_trace_2D.dat', unpack=True, usecols=[0,1])
+
+plt.plot(x, y, label="2D diffusion", color='brown', linestyle='solid', linewidth = 4, marker='^', markerfacecolor='brown', markeredgecolor="maroon", markersize=12)
+
+x, y = np.loadtxt('TSSA_tri_trace_0D.dat', unpack=True, usecols=[0,1])
+
+plt.plot(x, y, marker="*", markersize=15, markeredgecolor="black", markerfacecolor="black", label="No diffusion")
+
+plt.plot(x, y, marker="*", markersize=15, markeredgecolor="black", markerfacecolor="black")
+
+plt.xlabel('k', fontsize=18, weight='bold')
+plt.ylabel('Trace', fontsize=18, weight='bold')
+
+plt.xticks(fontsize=15, weight='bold')
+plt.yticks(fontsize=15, weight='bold')
+
+plt.rc('font', size=18)
+plt.rc('font', weight='bold')
+plt.legend(loc='best')
+plt.tight_layout()
+
+plt.savefig('TSSA_tri_trace.png', dpi=300)
---
+++ Analytical_codes_main/Figure 3/3J/3J-III/plot_lambda.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x, y = np.loadtxt('TSSA_tristable_lambdau3_1D.dat', unpack=True, usecols=[0,1])
+
+plt.plot(x, y, label="1D diffusion", color='blue', linestyle='solid', linewidth = 4, marker='o', markerfacecolor='blue', markeredgecolor="navy", markersize=12)
+
+x, y = np.loadtxt('TSSA_tristable_lambdau3_1D.dat', unpack=True, usecols=[0,2])
+
+plt.plot(x, y, color='blue', linestyle='solid', linewidth = 4, marker='o', markerfacecolor='blue', markeredgecolor="navy", markersize=12)
+
+x, y = np.loadtxt('TSSA_tristable_lambdau3_2D.dat', unpack=True, usecols=[0,1])
+
+plt.plot(x, y, label="2D diffusion", color='brown', linestyle='solid', linewidth = 4, marker='^', markerfacecolor='brown', markeredgecolor="maroon", markersize=12)
+
+x, y = np.loadtxt('TSSA_tristable_lambdau3_2D.dat', unpack=True, usecols=[0,2])
+
+plt.plot(x, y, color='brown', linestyle='solid', linewidth = 4, marker='^', markerfacecolor='brown', markeredgecolor="maroon", markersize=12)
+
+x, y = np.loadtxt('TSSA_tristable_lambdau3_0D.dat', unpack=True, usecols=[0,1])
+
+plt.plot(x, y, marker="*", markersize=15, markeredgecolor="black", markerfacecolor="black", label="No diffusion")
+
+x, y = np.loadtxt('TSSA_tristable_lambdau3_0D.dat', unpack=True, usecols=[0,2])
+
+plt.plot(x, y, marker="*", markersize=15, markeredgecolor="black", markerfacecolor="black")
+
+plt.xlabel('k', fontsize=18, weight='bold')
+plt.ylabel('Eigenvalues($\lambda_p, \lambda_m$)', fontsize=18, weight='bold')
+
+plt.xticks(fontsize=15, weight='bold')
+plt.yticks(fontsize=15, weight='bold')
+
+plt.rc('font', size=18)
+plt.rc('font', weight='bold')
+plt.legend(loc='best')
+plt.tight_layout()
+
+plt.xlim([-0.5, 11.2])
+
+plt.text(10.2, -0.115,'$\lambda_p$', color='blue')
+plt.text(10.2, -0.195,'$\lambda_m$', color='blue')
+plt.text(10.2, -0.215,'$\lambda_p$', color='brown')
+plt.text(10.2, -0.298,'$\lambda_m$', color='brown')
+
+plt.savefig('TSSA_tri_lambda.png', dpi=300)
+
